eb1fce4922db8178444991f1345e7cdaefad7095
scottlurowist <scottlurowist@gmail.com>
2020-08-27 15:06:55 -0400

    I AM COMITTING THE BETA OF THE HOME PAGE
    Currently HTML has a "home page section". This simply
    holds an image of a Tic Tic Toe board, and has two buttons
    to either create an account or to sign in to an existing
    account. When you click either button, an alert opens stating that
    the feature is not implemented.
    
    There is some very basic styling using SCSS, Flexbox, and Boostrap.
    The styling of the home page is one of the last things that I shall do.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,14 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const authEvents = require('./auth/events');
+
 
 $(() => {
-  // your JS code goes here
+
+  // Handles the two buttons found on the home page to either create an account
+  // or to sign into an existing account.
+  $('#home-page-create-account-button').on('click', authEvents.onNavigateoToSignUpPage);
+  $('#home-page-sign-in-button').on('click', authEvents.onNavigateToSignInPage);
+
 })
---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////
+// 
+// events.js
+//
+// This file handles events raised in the app.js file which is the entry
+// point for the application.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+const getFormFields = require('./../../../lib/get-form-fields');
+
+
+// Hadn
+const onNavigateoToSignUpPage = event => {
+
+    alert('Not yet implemented...')
+}
+
+
+const onNavigateToSignInPage = event => {
+ 
+    alert('Not yet implemented...')
+}
+
+
+module.exports = {
+    onNavigateoToSignUpPage: onNavigateoToSignUpPage,
+    onNavigateToSignInPage: onNavigateToSignInPage
+}
