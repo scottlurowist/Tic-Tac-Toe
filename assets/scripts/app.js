87debc24de743f32fc18bd7e2d89319881a29cfd
scottlurowist <scottlurowist@gmail.com>
2020-08-27 17:18:02 -0400

    I AM COMMITING A BETA OF THE CREATE ACCOUNT PAGE
    This page is just a beta. It makes a call to the sign-up web service.
    It then updates the status message area with a success or failure
    message.
    
    I added new files.
    
    The api.js file contains all of the web service invocations.
    
    I also added apiPromiseHandlers.js. This handles all of the
    success and failure callbacks to the web service promises.
    
    I also did some basic, boring, bootstrap styling that looks like
    every other website. Styling will be my last task.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,9 +10,18 @@ const authEvents = require('./auth/events');
 
 $(() => {
 
+  // We are making a single page app by storing all of our subviews in a single
+  // index.html file. So we imagine that we have a pseudo-state machine and 
+  // we must hide sections of the page depending upon which state we are.
+  // The state machine is implemented in ???.
+  $('#sign-up-page-form').hide();
+
   // Handles the two buttons found on the home page to either create an account
   // or to sign into an existing account.
   $('#home-page-create-account-button').on('click', authEvents.onNavigateoToSignUpPage);
   $('#home-page-sign-in-button').on('click', authEvents.onNavigateToSignInPage);
+
+  // This handles the button click on the create account page.
+  $('#sign-up-page-form').on('submit', authEvents.onSignUp);
 
 })
