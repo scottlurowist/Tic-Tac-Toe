0cd61ce7e02a337b928f12ec4c84435643c8217b
scottlurowist <scottlurowist@gmail.com>
2020-08-31 12:39:30 -0400

    I FIXED A BUG I FOUND IN GAME PLAY
    My image was scaled to be small. When testing that you cannot select
    a cell already played, I noticed that the correct thing happens When
    I click on the cell backgound DIV. But if I click on the image itself,
    Then I receive an "undefined" in the status message. I not check for
    a click on either the gamepiece image, or the Bootstrap grid cell
    directly. Both indicate that I cannot play the same cell again.

--- assets/scripts/gameScoringEngine.js
+++ assets/scripts/gameScoringEngine.js
@@ -2,7 +2,8 @@
 // 
 // gameScoringEngine.js
 //
-// This file 
+// This file keeps track of a Tic Tac Toe game. It keeps track of scoring,
+// manages some UI state.
 //
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -17,31 +18,39 @@ let currentEventTarget = null;
 let currentCellNumberClicked = -1;
 let isGameWon = false;
 
+// cache IDs for the board cells in an array for operations that require
+// styling every cell on the board with a forEach.
+const boardIds = ['#gameboard-cell-0', '#gameboard-cell-1', '#gameboard-cell-2',
+'#gameboard-cell-3', '#gameboard-cell-4', '#gameboard-cell-5',
+'#gameboard-cell-6', '#gameboard-cell-7', '#gameboard-cell-8' ]
+
 
 // This method should be invoked when the app is started or upon starting
 // a new game. It sets the initial state of the app to the home page.
 const initializeGameEngine = game => {
 
     currentGame = game;
+
+    // By requirements, the game starts with player X.
     currentPlayer = "x";
+
+    // Keep track of the number of moves.
     numberOfMoves = 0;
+
     currentEventTarget = null;
     currentCellNumberClicked = -1;
     isGameWon = false;    
 
-    const boardIds = ['#gameboard-cell-0', '#gameboard-cell-1', '#gameboard-cell-2',
-                      '#gameboard-cell-3', '#gameboard-cell-4', '#gameboard-cell-5',
-                      '#gameboard-cell-6', '#gameboard-cell-7', '#gameboard-cell-8' ]
-
-    // Reset the game board from possible previous games.
+    // Reset the game board from possible previous games. Clear the background
+    // color to white and remove any possible X and O images.
     boardIds.forEach(boardCell => {
 
       $(boardCell).attr("style", "background: white;")
       $(boardCell + " img").remove();
     });
 
-    $('#new-game-return-to-game-options').prop("disabled", true);
-    $('#new-game-within-game-board-button').prop("disabled", true);    
+    $('#new-game-return-to-game-options').hide();
+    $('#new-game-within-game-board-button').hide();    
 };
 
 
@@ -64,21 +73,21 @@ const checkBoardForWin = (firstCellIndex, secondCellIndex, thirdCellIndex) => {
       setTimeout(() => {
         document.querySelector(`#gameboard-cell-${firstCellIndex}`)
           .style.background = "yellow";
-      }, 750);
+      }, 500);
 
       setTimeout(() => {
         document.querySelector(`#gameboard-cell-${secondCellIndex}`)
           .style.background = "yellow";
-      }, 1500);
+      }, 1000);
 
       setTimeout(() => {
         document.querySelector(`#gameboard-cell-${thirdCellIndex}`)
           .style.background = "yellow";
-      }, 2250);
+      }, 1500);
 
       // Update the status area.
       $("#status-notification-message-area")
-      .text(`Player ${currentPlayer} wins!`); 
+        .text(`Player ${currentPlayer} wins in ${numberOfMoves} moves!`); 
 
       return true;
     }
@@ -143,8 +152,8 @@ const checkForWin = () => {
 
   if (isThereAWin) {
       
-    $('#new-game-return-to-game-options').prop("disabled", false);
-    $('#new-game-within-game-board-button').prop("disabled", false);  
+    $('#new-game-return-to-game-options').show();
+    $('#new-game-within-game-board-button').show();  
   }
 
   return isThereAWin;
@@ -158,9 +167,14 @@ const checkForTie = () => {
     if (numberOfMoves === 9) {
         $("#status-notification-message-area")
         .text(`Players X and O are tied.`);  
+
+        boardIds.forEach(boardCell => {
+
+          $(boardCell).attr("style", "background: red;");
+        });
         
-        $('#new-game-return-to-game-options').prop("disabled", false);
-        $('#new-game-within-game-board-button').prop("disabled", false);  
+        $('#new-game-return-to-game-options').show();
+        $('#new-game-within-game-board-button').show();  
         
         return true;
     }
@@ -177,24 +191,37 @@ const isThereAWinOrTie = () => {
 };
 
 
-// Navigates to the next "page" in the SPA, as defined by nextState.
+// Processes the current user selection to make sure that the user hasn't
+// selected a game cell already played. It also checks whether the user 
+// has won or tied so that the web service may be properly informed.
 const processCurrentMove = event => {
 
+    // Each board cell's ID terminates with the cell #. Simply
+    // extract it from the ID so that the game engine knows what
+    // was selected.
     currentCellNumberClicked = event.target.id[event.target.id.length - 1];
 
     $('#status-notification-message-area').text('');
 
     // Did the user select a cell that has already been played?
+    if ((event.target.className === "x-image") ||
+        (event.target.className === "o-image")) {
+      $('#status-notification-message-area')
+      .text('You cannot select a cell already played. Try again.');
+
+      return;
+    }
+
     if (currentGame.cells[currentCellNumberClicked] === "x"||
         currentGame.cells[currentCellNumberClicked] === "o") {
 
-        $('#status-notification-message-area')
-        .text('You cannot select a cell already played. Try again.');
-
-        return;
+          $('#status-notification-message-area')
+          .text('You cannot select a cell already played. Try again.');
+    
+          return;
     }
 
-    // Save this for processing in updateGameStatus.
+    // Save the board cell clicked for processing in updateGameStatus.
     currentEventTarget = event.target;
 
     // Update the game board for scoring purposes.
@@ -230,20 +257,21 @@ const updateGameStatus = (game, wasUpdateSuccessful) => {
    // When we load the images into the grid and hide the grid then reshow 
    // the grid, the images appear to unload. So we hide our X and Y images
    // and then clone them and then attach them to the clicked cell grid. 
-   let imageName = '#x-image';
+   let imageName = '.x-image';
 
    if (currentPlayer === 'o') {
-     imageName = '#o-image';     
+     imageName = '.o-image';     
    }
 
-   $(imageName).clone().appendTo($(currentEventTarget)); 
+   // Clone one of our hidden X and O images and attach to the board cell.
+   $(`#gameboard-images-cache ${imageName}`).clone().appendTo($(currentEventTarget)); 
 
    // Now we need to show the hidden, cloned image.
    $(currentEventTarget).children("img").css("display", "inline");
 
    if (!isGameWon) {
-    $("#status-notification-message-area")
-    .text(`Player ${currentPlayer} to cell ${currentCellNumberClicked}.`);
+     $("#status-notification-message-area")
+      .text(`Player ${currentPlayer} to cell ${currentCellNumberClicked}.`);
    }
 
    // Switch player X to O, or O to X.
